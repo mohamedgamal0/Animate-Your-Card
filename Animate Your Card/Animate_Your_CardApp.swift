601135917cf777b27db89b9d8f8c17a2aecf1855
mohamed gamal <mohamed.gamal@areebgroup.com>
2022-02-14 16:34:51 +0200

    Initial Commit

---
+++ Animate Your Card/Animate_Your_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animate_Your_CardApp.swift
+//  Animate Your Card
+//
+//  Created by Mohamed Gamal on 14/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct Animate_Your_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
